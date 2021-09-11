dfe5208ac482b1d0e3a05a7eccbd53b7c6fb3892
Puttipong Kongpoung <leoputti@hotmail.com>
2021-09-11 16:54:22 +0700

    1st commit

---
+++ RolePlayingGame.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RpgLibrary
+{
+    public class RolePlayingGame
+    {
+        string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        string description;
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+        private RolePlayingGame()
+        {
+
+        }
+        public RolePlayingGame(string name,string description)
+        {
+            Name = name;
+            Description = description;
+        }
+    }
+}
